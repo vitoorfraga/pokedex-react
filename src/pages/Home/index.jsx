d1d3d84037ce56830080852a5769236deca1611e
Vitor Fraga <vitorfragaps@gmail.com>
2022-11-03 13:36:58 -0300

    Teste 2

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import Header from "../../components/Header";
 
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+
+import awsconfig from './aws-exports';
+
+import "@aws-amplify/ui-react/styles.css";
+import { studioTheme } from "./ui-components";
+
+Amplify.configure(awsconfig);
+
 import { 
   Ampligram 
 } from './ui-components';
@@ -28,6 +38,7 @@ const Home = () => {
 
       <div className="pokemon-list">
       <div>
+        <h1> Teste Teste Teste</h1>
       <ActionCardWithSlot
       {/*Add content to the mainText prop*/}
         mainText={
